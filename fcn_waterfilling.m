5a31d98902c5c61e7b7ce71d2f488d259ea85332
4llex <alexr@gec.inatel.br>
2019-10-25 17:41:14 -0300

    V6 static allocation by wheberth

---
+++ fcn_waterfilling.m
@@ -0,0 +1,27 @@
+function [ subPower, subAlloc, subCapacity ] = fcn_waterfilling(Power,SigmaSqr,Gamma,H,subAlloc)
+% -------------------------------------------------------------------------
+% This function distributes the specified amount of power among the
+% subcarriers using the waterfilling algorithm
+% -------------------------------------------------------------------------
+%   Power    : power to be distributed among the subcarriers
+%   SigmaSqr : noise variance
+%   subGains : vector with tha channel gain at each subcarrier
+%   TargetSer: target ser
+%   subAlloc : boolean vector indicating which subcarriers are allocated
+%   subPower : vector indicating the power allocated to each subcarrier
+
+
+
+AllowedSubCarriers=subAlloc;
+subPower=ones(size(subAlloc))*-1;
+while(sum(subPower<0))
+    subPower(subPower<0)=0;
+    idx = find(subAlloc>0);
+    subPower(idx)=(Power/sum(subAlloc))...
+        + ((Gamma*SigmaSqr)/sum(subAlloc))*sum(abs(H(idx)).^-2)...
+        - (Gamma*SigmaSqr)*(abs(H(idx)).^-2);
+    subAlloc(subPower<0)=0;
+end
+%subAlloc=AllowedSubCarriers;
+%subCapacity = log2(1 + (subPower.*(abs(H).^2))/(Gamma*SigmaSqr));
+subCapacity = floor(log2(1 + (subPower.*(abs(H).^2))/(Gamma*SigmaSqr)));  % Modulation constraint
---
+++ staticAllocation_v6_3users.m
@@ -0,0 +1,113 @@
+%%% Simulação de alocação estatica de usuarios em simbolo OFDM
+%%% OFDMA with static allocation
+
+TargetSer = 1e-3;                           %% SER Alvo
+SNR = 5:2:30;                               %% XXX
+N = 1584;                                   %% Numero de Subportadoras
+b = zeros(1,N);                             %% Vetor de Bits das portadoras / Numerologia 3
+Total_bits = zeros(1,length(SNR));          %% Total de bits em um simbolo
+bits_per_rb = zeros(1,length(SNR));         %% qtd media de Bits por RB 
+quantizar = 'yes';                          %%
+RB = 132;                                   %% qtd de RB
+sc_per_rb = 12;                             %% SubCarriers per RB, depends numerology    
+%% SNR gap para constelação M-QAM:
+Gamma=(1/3)*qfuncinv(TargetSer/4)^2; % Gap to channel capacity M-QAM
+%% Calculo da Potencia Maxima por Usuario
+
+
+%% 
+%subPower = 20/1854; % 20 seria a potencia max do sistema de transmissao
+% LTE EVA CHANNEL
+freq_sample = 23.76e6;     %N*15e3; %30.72e6; sample rate do LTE
+EVA_SR3072_Delay           =[0 30 150 310 370 710 1090 1730 2510].*1e-9;
+EVA_SR3072_PowerdB_Gain    = [0 -1.5 -1.4 -3.6 -0.6 -9.1 -7 -12 -16.9]; %  20*log10(0.39)= -8.1787 => -8.1787 dB -> Voltage-ratio = 0.398107
+
+chan_EVA = rayleighchan((1/(freq_sample)),0,EVA_SR3072_Delay,EVA_SR3072_PowerdB_Gain);        
+impulse= [1; zeros(N - 1,1)];  
+
+mask1 = [ones(1, 44), zeros(1, 132-44)];
+mask2 = circshift(mask1, 44);
+mask3 = circshift(mask2, 44);
+
+
+%num_itr = 5000;
+num_itr = 1000;
+for i=1:length(SNR)
+    i
+    j=0;
+    while j<num_itr 
+        %H = ones(1,N);%% H ideal
+        h1 = filter(chan_EVA, impulse)';
+        %H1 = fft(h1,N/3);
+        H1 = fft(h1,N);
+        %reset(chan_EVA);
+        
+        h2 = filter(chan_EVA, impulse)';
+        %H2 = fft(h2,N/3);
+        H2 = fft(h2,N);
+        %reset(chan_EVA);
+        
+        h3 = filter(chan_EVA, impulse)';
+        %H3 = fft(h3,N/3);
+        H3 = fft(h3,N);
+        %reset(chan_EVA);
+        
+        % Calcula Resposta em frequencia média para os 132 RB's
+        H1 = rb_h_media(H1, sc_per_rb);
+        H2 = rb_h_media(H2, sc_per_rb);
+        H3 = rb_h_media(H3, sc_per_rb);
+        SNRLIN = 10^(SNR(i)/10);
+        P  = 132;
+        Pu = P/3;
+        
+        [subPower1,~, subCapacity1 ] = fcn_waterfilling(Pu, P/(SNRLIN*RB), Gamma, H1, mask1);
+        [subPower2,~, subCapacity2 ] = fcn_waterfilling(Pu, P/(SNRLIN*RB), Gamma, H2, mask2);
+        [subPower3,~, subCapacity3 ] = fcn_waterfilling(Pu, P/(SNRLIN*RB), Gamma, H3, mask3);
+    
+        b = subCapacity1 + subCapacity2 +  subCapacity3;
+
+        % Quantização
+        if strcmp(quantizar,'yes')
+            b(b<2) = 0;
+            b((b>2)&(b<4)) = 2;
+            b((b>4)&(b<6)) = 4;
+            b((b>6)&(b<8)) = 6;
+            b(b>8) = 8;
+        end
+
+%         figure(2);
+%         cla;
+%         plot(subPower1); hold on; plot(subPower2); plot(subPower3);
+%         drawnow();
+
+%         figure(3);
+%         cla;
+%         plot(abs(H1)); hold on; plot(abs(H2)); plot(abs(H3));
+%         drawnow();
+%         pause(1);
+        
+        Total_bits(i) = Total_bits(i) + sum(b);
+        %bm(j) = b;
+        
+        j = j+1;
+    end  
+    
+    Total_bits(i) = Total_bits(i)/num_itr;
+    
+    bits_per_rb(i) = (Total_bits(i)/RB)*12; 
+end
+
+% Saving Vector in a File
+ Sim.DataSNR = SNR;   
+ Sim.DataBPRB = bits_per_rb;
+ FileName = strcat('SIM_h2.mat'); 
+ save(FileName,'Sim');  
+
+%% Gera graficos de Bits/SNR
+figure;
+plot(SNR, bits_per_rb, '-xr');
+title('Alocação Estática em sistema de multiplo acesso Ortogonal');
+xlabel('SNR [dB]'); 
+ylabel('Bits/RB'); 
+grid on;
+grid minor;
